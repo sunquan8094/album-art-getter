65d226daeddd92158bed237b8c1e0ff2eadb2a36
dchan3 <dchan3@hawaii.edu>
2017-02-20 12:55:08 -1000

    Added invalid parameter tests

--- spec/parameter_spec.js
+++ spec/parameter_spec.js
@@ -1,16 +1,44 @@
 var main = require("../main.js");
 
 describe("Parameter tests", function() {
-  describe("Fewer than 4 parameters", function() {
+  describe("Fewer than 4 parameters - 2", function() {
     it("returns false", function(done) {
       expect(main.validateParams(["node", "main.js"])).toBe(false);
       done();
     });
   });
 
-  describe("More than 5 parameters", function() {
+  describe("Fewer than 4 parameters - 3", function() {
+    it("returns false", function(done) {
+      expect(main.validateParams(["node", "main.js", "Bass Modulators"])).toBe(false);
+      done();
+    });
+  });
+
+  describe("Minimum number of valid parameters", function() {
+    it("returns true", function(done) {
+      expect(main.validateParams(["node", "main.js", "Da Tweekaz", "See The Light"])).toBe(true);
+      done();
+    });
+  });
+
+  describe("More than 5 parameters - 6", function() {
     it("returns false", function(done) {
       expect(main.validateParams(["node", "main.js", "Wasted Penguinz", "Magic", "Clarity", "2016"])).toBe(false);
+      done();
+    });
+  });
+
+  describe("Invalid parameter - decimal pixels", function() {
+    it("returns false", function(done) {
+      expect(main.validateParams(["node", "main.js", "Headhunterz", "Dragonborn", "678.902px"])).toBe(false);
+      done();
+    });
+  });
+
+  describe("Invalid parameter - NaN", function() {
+    it("returns false", function(done) {
+        expect(main.validateParams(["node", "main.js", "Coone", "Robotz", "infinitely huge"])).toBe(false);
       done();
     });
   });
