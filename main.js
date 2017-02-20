8ae84755601ea4c774f6fac1e47a5ec4f05e72f5
dchan3 <dchan3@hawaii.edu>
2017-02-20 01:38:00 -1000

    Image manipulation works correctly

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var xml2js = require('xml2js');
 var child_process = require('child_process');
+var Jimp = require("jimp");
 
 var exports = module.exports = {};
 
@@ -12,16 +13,19 @@ exports.validateParams = function(params) {
   }
 
   if (params.length === 5) {
-    if (!params[4].endsWith(/(px)|x/i)) {
+    if (!params[4].endsWith("px") && !params[4].endsWith("x")) {
       console.log("Error: Size in wrong format.");
       return false;
     }
-    if (Number(params[4].substring(0, params[4].indexOf(/[px]/i))) !== NaN) {
-      if (size <= 0) {
+
+    var endOfNum = (params[4].indexOf("p") > -1) ? params[4].indexOf("p") : params[4].indexOf("x");
+
+    if (Number(params[4].substring(0, endOfNum)) !== NaN) {
+      if (Number(params[4].substring(0, endOfNum)) <= 0) {
         console.log("Error: Size must be nonzero and nonnegative.");
         return false;
       }
-      if (resize_type === "px" && Math.floor(size) < size) {
+      if (params[4].endsWith("px") && Math.floor(Number(params[4].substring(0, endOfNum))) < Number(params[4].substring(0, endOfNum))) {
         console.log("Error: Size must be an integer for pixels.");
         return false;
       }
@@ -33,9 +37,12 @@ exports.validateParams = function(params) {
   }
   return true;
 }
+
 exports.getPictures = function(params) {
   // Get command line arguments
-  var artist = params[2], song = params[3], size = (params.length === 5) ? Number(params[4].substring(0, params[4].indexOf(/[px]/i))) : 0, resize_type = (params.length === 5) ? (params[4].endsWith('px') ? "px" : "x") : "";
+
+  var artist = params[2], song = params[3], endOfNum = (params.length === 5) ? (params[4].indexOf("p") > -1) ? params[4].indexOf("p") : params[4].indexOf("x") : 0,
+      size = (params.length === 5) ? Number(params[4].substring(0, endOfNum)) : 0, resize_type = (params.length === 5) ? (params[4].endsWith('px') ? "px" : "x") : "";
 
   // Make request
   var req = http.get('http://api.lololyrics.com/0.5/getLyric?artist=' + encodeURIComponent(artist) + '&track=' + encodeURIComponent(song), (res) => {
@@ -55,8 +62,25 @@ exports.getPictures = function(params) {
         console.log(result.result.cover[0]);
         if (result.result.status[0] === 'OK') {
           console.log("Status OK. Obtaining album art...");
-          child_process.exec('curl ' + result.result.cover[0] + ' -o "' + artist + ' - ' + song + '.jpeg"', function(error, stdout, stderr) {
-            if (err) { console.error(err); return true;}
+          Jimp.read(result.result.cover[0], function(err, img) {
+            if (err) {
+              console.error(err); return false;
+            }
+            if (size !== 0) {
+              console.log("Resizing image...");
+              console.log(size);
+              if (resize_type === "px") {
+                img.clone().resize(size,Jimp.AUTO).quality(60).write(artist + ' - ' + song + "_" + params[4] + '.jpeg');
+              }
+              else if (resize_type === "x") {
+                img.clone().scale(size).quality(100).write(artist + ' - ' + song + "_" + params[4] + '.jpeg');
+              }
+            }
+            else img.clone().write(artist + ' - ' + song + '.jpeg');
+          }).catch(function(err) {
+            console.error(err);
+            console.log("There was an error processing your image.");
+            return false;
           });
           console.log("Album art obtained.");
           return true;
