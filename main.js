caea38e498aab7fedd16f89f2a2f24fdce4be6ee
dchan3 <dchan3@hawaii.edu>
2017-02-20 20:51:17 -1000

    Added support for the Spotify API

--- main.js
+++ main.js
@@ -1,48 +1,94 @@
 var http = require('http');
+var https = require('https');
 var xml2js = require('xml2js');
 var child_process = require('child_process');
 var Jimp = require("jimp");
 
 var exports = module.exports = {};
 
+exports.artist = "";
+exports.song = "";
+exports.size = 0;
+exports.size_param_index = -1;
+exports.api_index = -1;
+exports.endOfNum = 0;
+exports.size_param = "";
+exports.api_param = "";
+exports.resize_type = "";
+
+exports.firstIndexOfRegexInArray = function(array, regex) {
+  for (var a = 0; a < array.length; a++) {
+    if (regex.test(array[a])) {
+      return a;
+    }
+  }
+  return -1;
+}
+
 exports.validateParams = function(params) {
   // Print usage if argument count there are fewer than 4 arguments or more than 5 arguments and exit
-  if (params.length < 4 || params.length > 5) {
-    console.log("Usage: <artist> <song name> [<new size in pixels>px | <scale>x]");
+  if (params.length < 4 || params.length > 6) {
+    console.log("Usage: <artist> <song name> [ <new size in pixels>px | <scale>x ] [-<1 (default) | 2>]" );
     return false;
   }
 
-  if (params.length === 5) {
-    if (!(/^\d+(\.\d+)?p?x/.test(params[4]))) {
-      console.log("Error: Invalid size parameter.");
-      return false;
-    }
+  exports.artist = params[2];
+  exports.song = params[3];
 
-    var endOfNum = (params[4].indexOf("p") > -1) ? params[4].indexOf("p") : params[4].indexOf("x");
+  exports.size_param_index = exports.firstIndexOfRegexInArray(params, /^\d+(\.\d+)?p?x$/);
+  exports.api_index = exports.firstIndexOfRegexInArray(params, /^\-[12]$/);
 
-    if (Number(params[4].substring(0, endOfNum)) !== NaN) {
-      if (params[4].endsWith("px") && Math.floor(Number(params[4].substring(0, endOfNum))) < Number(params[4].substring(0, endOfNum))) {
+  if (exports.size_param_index <= -1 && (!(params.length == 5 && exports.api_index > -1))) {
+    console.log("Error: Invalid size parameter.");
+    return false;
+  }
+  else if (exports.size_param_index > -1){
+    exports.size_param = params[exports.size_param_index];
+    exports.endOfNum = (exports.size_param.indexOf("p") > -1) ? exports.size_param.indexOf("p") : exports.size_param.indexOf("x");
+
+    if (Number(exports.size_param.substring(0, endOfNum)) !== NaN) {
+      if (exports.size_param.endsWith("px") && Math.floor(Number(exports.size_param.substring(0, endOfNum))) < Number(exports.size_param.substring(0, endOfNum))) {
         console.log("Error: Size must be an integer for pixels.");
         return false;
       }
     }
+
+    exports.resize_type = (exports.size_param.indexOf("p") > -1) ? "px" : "x";
   }
+
+  if (exports.api_index <= -1 && (!(params.length == 5 && exports.size_param_index > -1))) {
+    console.log("Error: Invalid API mode parameter.");
+    return false;
+  }
+  else if (exports.api_index > -1) {
+    exports.api_param = params[exports.api_index];
+    exports.api_mode = Number(exports.api_param.substring(1));
+  }
+
   return true;
 }
 
-exports.getPictures = function(params) {
-  // Get command line arguments
+exports.manipulateImage = function(err, img) {
+  if (err) {
+    console.error(err); return false;
+  }
+  if (exports.size !== 0) {
+    console.log("Resizing image...");
+    if (exports.resize_type === "px") {
+      img.clone().resize(exports.size,Jimp.AUTO).quality(60).write(exports.artist + ' - ' + exports.song + "_" + exports.size_param + '.jpeg');
+    }
+    else if (exports.resize_type === "x") {
+      img.clone().scale(exports.size).quality(100).write(exports.artist + ' - ' + exports.song + "_" + exports.size_param + '.jpeg');
+    }
+  }
+  else img.clone().write(exports.artist + ' - ' + exports.song + '.jpeg');
+  return true;
+}
 
-  var artist = params[2], song = params[3], endOfNum = (params.length === 5) ? (params[4].indexOf("p") > -1) ? params[4].indexOf("p") : params[4].indexOf("x") : 0,
-      size = (params.length === 5) ? Number(params[4].substring(0, endOfNum)) : 0, resize_type = (params.length === 5) ? (params[4].endsWith('px') ? "px" : "x") : "";
-
-  // Make request
-  var req = http.get('http://api.lololyrics.com/0.5/getLyric?artist=' + encodeURIComponent(artist) + '&track=' + encodeURIComponent(song), (res) => {
-
-  });
-
-  // Make strings for storing raw and parsed responses
+exports.getPictures = function() {
+  // Make strings for storing raw and parsed responses as well as request
   var doc = "", parsed = "";
+  var req = (exports.api_mode === 1) ? http.get('http://api.lololyrics.com/0.5/getLyric?artist=' + encodeURIComponent(exports.artist) + '&track=' + encodeURIComponent(exports.song), (res) => {}) : https.get('https://api.spotify.com/v1/search?q='  + encodeURIComponent(exports.artist + " " + exports.song) + "&type=track", (res) => {});
 
   req.on('response', function(response) {
     response.on('data', function(chunk) {
@@ -50,38 +96,36 @@ exports.getPictures = function(params) {
     });
 
     response.on('end', function() {
-      parsed = xml2js.parseString(doc, function(err, result) {
-        console.log(result.result.cover[0]);
-        if (result.result.status[0] === 'OK') {
-          console.log("Status OK. Obtaining album art...");
-          Jimp.read(result.result.cover[0], function(err, img) {
-            if (err) {
-              console.error(err); return false;
-            }
-            if (size !== 0) {
-              console.log("Resizing image...");
-              console.log(size);
-              if (resize_type === "px") {
-                img.clone().resize(size,Jimp.AUTO).quality(60).write(artist + ' - ' + song + "_" + params[4] + '.jpeg');
-              }
-              else if (resize_type === "x") {
-                img.clone().scale(size).quality(100).write(artist + ' - ' + song + "_" + params[4] + '.jpeg');
-              }
-            }
-            else img.clone().write(artist + ' - ' + song + '.jpeg');
-          }).catch(function(err) {
-            console.error(err);
-            console.log("There was an error processing your image.");
+      if (exports.api_mode === 1) {
+        parsed = xml2js.parseString(doc, function(err, result) {
+          console.log(result);
+          if (result.result.status[0] === 'OK') {
+            console.log("Status OK. Obtaining album art...");
+            Jimp.read(result.result.cover[0], exports.manipulateImage).catch(function(err) {
+              console.error(err);
+              console.log("There was an error processing your image.");
+              return false;
+            });
+            console.log("Album art obtained.");
+            return true;
+          }
+          else if (result.result.status[0] === 'ERROR') {
+            console.error("There was an error.");
             return false;
-          });
-          console.log("Album art obtained.");
-          return true;
-        }
-        else if (result.result.status[0] === 'ERROR') {
-          console.error("There was an error.");
+          }
+        });
+      }
+      else if (exports.api_mode === 2) {
+        parsed = JSON.parse(doc);
+        console.log(parsed.tracks.items);
+        Jimp.read(parsed.tracks.items[0].album.images[0].url, exports.manipulateImage).catch(function(err) {
+          console.error(err);
+          console.log("There was an error processing your image.");
           return false;
-        }
-      });
+        });
+        console.log("Album art obtained.");
+        return true;
+      }
     });
   });
 }
