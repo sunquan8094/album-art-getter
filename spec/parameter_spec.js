a6d0ea1314e0ee13d72eeb9c6d8f9c840fa1c522
dchan3 <dchan3@hawaii.edu>
2017-02-21 14:07:47 -1000

    Added more tests

--- spec/parameter_spec.js
+++ spec/parameter_spec.js
@@ -1,45 +1,63 @@
 var main = require("../main.js");
 
 describe("Parameter tests", function() {
-  describe("Fewer than 4 parameters - 2", function() {
-    it("returns false", function(done) {
-      expect(main.validateParams(["node", "main.js"])).toBe(false);
-      done();
+  describe("Parameter count tests", function() {
+    describe("Fewer than 4 parameters - 2", function() {
+      it("returns false", function(done) {
+        expect(main.validateParams(["node", "main.js"])).toBe(false);
+        done();
+      });
+    });
+
+    describe("Fewer than 4 parameters - 3", function() {
+      it("returns false", function(done) {
+        expect(main.validateParams(["node", "main.js", "Bass Modulators"])).toBe(false);
+        done();
+      });
+    });
+
+    describe("Minimum number of valid parameters", function() {
+      it("returns true", function(done) {
+        expect(main.validateParams(["node", "main.js", "Da Tweekaz", "See The Light"])).toBe(true);
+        done();
+      });
+    });
+
+    describe("More than 6 parameters - 7", function() {
+      it("returns false", function(done) {
+        expect(main.validateParams(["node", "main.js", "Wasted Penguinz", "Magic", "Clarity", "2016", "Dirty Workz"])).toBe(false);
+        done();
+      });
     });
   });
-
-  describe("Fewer than 4 parameters - 3", function() {
-    it("returns false", function(done) {
-      expect(main.validateParams(["node", "main.js", "Bass Modulators"])).toBe(false);
-      done();
+  describe("Invalid size parameter", function() {
+    describe("Invalid size parameter - decimal pixels", function() {
+      it("returns false", function(done) {
+        expect(main.validateParams(["node", "main.js", "Headhunterz", "Dragonborn", "678.902px"])).toBe(false);
+        done();
+      });
     });
-  });
 
-  describe("Minimum number of valid parameters", function() {
-    it("returns true", function(done) {
-      expect(main.validateParams(["node", "main.js", "Da Tweekaz", "See The Light"])).toBe(true);
-      done();
-    });
-  });
-
-  describe("More than 6 parameters - 7", function() {
-    it("returns false", function(done) {
-      expect(main.validateParams(["node", "main.js", "Wasted Penguinz", "Magic", "Clarity", "2016", "Dirty Workz"])).toBe(false);
-      done();
-    });
-  });
-
-  describe("Invalid size parameter - decimal pixels", function() {
-    it("returns false", function(done) {
-      expect(main.validateParams(["node", "main.js", "Headhunterz", "Dragonborn", "678.902px"])).toBe(false);
-      done();
-    });
-  });
-
-  describe("Invalid size parameter - NaN", function() {
-    it("returns false", function(done) {
+    describe("Invalid size parameter - NaN", function() {
+      it("returns false", function(done) {
         expect(main.validateParams(["node", "main.js", "Coone", "Robotz", "infinitely huge"])).toBe(false);
-      done();
+        done();
+      });
+    });
+  });
+  describe("Invalid API mode flag", function() {
+    describe("Invalid API mode flag - numeral character other than one and two", function() {
+      it("returns false", function(done) {
+        expect(main.validateParams(["node", "main.js", "For the Fallen Dreams", "The Big Empty", "-3"])).toBe(false);
+        done();
+      });
+    });
+
+    describe("Invalid API mode flag - one nonnumeric character", function() {
+      it("returns false", function(done) {
+        expect(main.validateParams(["node", "main.js", "The Color Morale", "Keep Me In My Body", "-p"])).toBe(false);
+        done();
+      });
     });
   });
 });
